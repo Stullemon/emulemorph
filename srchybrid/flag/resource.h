7c6adf2cb4c754fa5c4a7f056fa4bc9f0be4119b
andcycle <andcycle>
2004-02-05 15:36:11 +0000

    update flag, correct GB and UK

--- srchybrid/flag/resource.h
+++ srchybrid/flag/resource.h
@@ -240,12 +240,14 @@
 #define IDI_COUNTRY_FLAG_ZA             336
 #define IDI_COUNTRY_FLAG_ZM             337
 #define IDI_COUNTRY_FLAG_ZW             338
+#define IDI_ICON1                       340
+#define IDI_COUNTRY_FLAG_UK             340
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        340
+#define _APS_NEXT_RESOURCE_VALUE        341
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
