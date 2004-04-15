c06569daeabf1d263bdc98419394e68575799ac4
andcycle <andcycle>
2004-04-15 07:33:49 +0000

    fix bug in reduce a little CPU usage for ratio count

--- srchybrid/ClientCredits.cpp
+++ srchybrid/ClientCredits.cpp
@@ -53,8 +53,8 @@ CClientCredits::CClientCredits(CreditStruct* in_credits)
 	m_fLastScoreRatio = 0;
 	m_cssCurrentCreditSystem = thePrefs.GetCreditSystem();
 	//Morph End - Added by AndCycle, reduce a little CPU usage for ratio count
-	InitPayBackFirstStatus();//EastShare - added by AndCycle, Pay Back First
 
+	InitPayBackFirstStatus();//EastShare - added by AndCycle, Pay Back First
 }
 
 CClientCredits::CClientCredits(const uchar* key)
@@ -72,8 +72,8 @@ CClientCredits::CClientCredits(const uchar* key)
 	m_fLastScoreRatio = 0;
 	m_cssCurrentCreditSystem = thePrefs.GetCreditSystem();
 	//Morph End - Added by AndCycle, reduce a little CPU usage for ratio count
-	InitPayBackFirstStatus();//EastShare - added by AndCycle, Pay Back First
 
+	InitPayBackFirstStatus();//EastShare - added by AndCycle, Pay Back First
 }
 
 CClientCredits::~CClientCredits()
@@ -127,6 +127,12 @@ uint64	CClientCredits::GetDownloadedTotal(){
 
 float CClientCredits::GetScoreRatio(uint32 dwForIP)
 {
+	// check the client ident status
+	if ( ( GetCurrentIdentState(dwForIP) == IS_IDFAILED || GetCurrentIdentState(dwForIP) == IS_IDBADGUY || GetCurrentIdentState(dwForIP) == IS_IDNEEDED) && theApp.clientcredits->CryptoAvailable() ){
+		// bad guy - no credits for you
+		return 1;
+	}
+
 	//Morph Start - Added by AndCycle, reduce a little CPU usage for ratio count
 	if(m_cssCurrentCreditSystem != thePrefs.GetCreditSystem()){
 		m_cssCurrentCreditSystem = thePrefs.GetCreditSystem();
@@ -135,12 +141,6 @@ float CClientCredits::GetScoreRatio(uint32 dwForIP)
 	}
 	m_bCheckScoreRatio = false;
 	//Morph End - Added by AndCycle, reduce a little CPU usage for ratio count
-
-	// check the client ident status
-	if ( ( GetCurrentIdentState(dwForIP) == IS_IDFAILED || GetCurrentIdentState(dwForIP) == IS_IDBADGUY || GetCurrentIdentState(dwForIP) == IS_IDNEEDED) && theApp.clientcredits->CryptoAvailable() ){
-		// bad guy - no credits for you
-		return m_fLastScoreRatio = 1;
-	}
 
 	//Morph Start - Modified by AndCycle, reduce a little CPU usage for ratio count
 
@@ -199,7 +199,7 @@ float CClientCredits::GetScoreRatio(uint32 dwForIP)
 
 		// EastShare START - Added by TAHO, new Credit System //Modified by Pretender
 		case CS_EASTSHARE:{
-			result = ((m_pCredits->nKeySize == 0)||(GetCurrentIdentState(dwForIP)!=IS_IDENTIFIED)) ? 80 : 100;
+			result = (IdentState == IS_NOTAVAILABLE) ? 80 : 100;
 			
 /*			if (GetDownloadedTotal() < GetUploadedTotal())
 				result += (float)(((double)GetDownloadedTotal()-(double)GetUploadedTotal())/524288.0 + (double)pow((double)GetDownloadedTotal()/1048576.0,0.7));
@@ -675,13 +675,11 @@ void CClientCredits::InitalizeIdent(){
 		memset(m_abyPublicKey,0,80); // for debugging
 		m_nPublicKeyLen = 0;
 		IdentState = IS_NOTAVAILABLE;
-		m_bCheckScoreRatio = true; //Morph - Added by SiRoB, reduce a little CPU usage for ratio count (Hotfix)
 	}
 	else{
 		m_nPublicKeyLen = m_pCredits->nKeySize;
 		memcpy(m_abyPublicKey, m_pCredits->abySecureIdent, m_nPublicKeyLen);
 		IdentState = IS_IDNEEDED;
-		m_bCheckScoreRatio = true; //Morph - Added by SiRoB, reduce a little CPU usage for ratio count (Hotfix)
 	}
 	m_dwCryptRndChallengeFor = 0;
 	m_dwCryptRndChallengeFrom = 0;
@@ -705,7 +703,6 @@ void CClientCredits::Verified(uint32 dwForIP){
 		}
 	}
 	IdentState = IS_IDENTIFIED;
-	m_bCheckScoreRatio = true; //Morph - Added by SiRoB, reduce a little CPU usage for ratio count (Hotfix)
 }
 
 bool CClientCredits::SetSecureIdent(uchar* pachIdent, uint8 nIdentLen){ // verified Public key cannot change, use only if there is not public key yet
@@ -714,7 +711,6 @@ bool CClientCredits::SetSecureIdent(uchar* pachIdent, uint8 nIdentLen){ // verif
 	memcpy(m_abyPublicKey,pachIdent, nIdentLen);
 	m_nPublicKeyLen = nIdentLen;
 	IdentState = IS_IDNEEDED;
-	m_bCheckScoreRatio = true; //Morph - Added by SiRoB, reduce a little CPU usage for ratio count (Hotfix)
 	return true;
 }
 
