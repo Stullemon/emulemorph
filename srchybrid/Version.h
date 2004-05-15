a48f73cfa00a7651743a5123675a1a546ef4852d
sirob <sirob>
2004-05-15 18:16:22 +0000

    Freeze due to non thread safe call (big Thank's to zz)

--- srchybrid/Version.h
+++ srchybrid/Version.h
@@ -46,6 +46,6 @@
   #endif
 #endif
 
-#define MOD_VERSION		_T("MorphXT v2.11") //MORPH - Added by SiRoB
+#define MOD_VERSION		_T("MorphXT v3.0") //MORPH - Added by SiRoB
 #define	SZ_VERSION_NAME		chSTR(VERSION_MJR) _T(".") chSTR(VERSION_MIN) _T(".") chSTR(VERSION_UPDATE) _T(" [") MOD_VERSION _T("]")
 #endif /* !__VERSION_H__ */
