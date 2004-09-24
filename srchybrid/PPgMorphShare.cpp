f73f4a208939f35ad78e03f412cabd18425be3c5
sirob <sirob>
2004-09-24 13:55:58 +0000

    Added Moph Share PPg

---
+++ srchybrid/PPgMorphShare.cpp
@@ -0,0 +1,267 @@
+// PpgMorph.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "emule.h"
+#include "PPgMorphShare.h"
+#include "emuledlg.h"
+#include "serverWnd.h" //MORPH - Added by SiRoB
+#include "OtherFunctions.h"
+#include "Scheduler.h" //MORPH - Added by SiRoB, Fix for Param used in scheduler
+#include "searchDlg.h"
+#include "sharedfilelist.h" //MORPH - Added by SiRoB, POWERSHARE Limit
+#ifdef _DEBUG
+#undef THIS_FILE
+static char THIS_FILE[]=__FILE__;
+#define new DEBUG_NEW
+#endif
+
+
+///////////////////////////////////////////////////////////////////////////////
+// CPPgMorphShare dialog
+
+IMPLEMENT_DYNAMIC(CPPgMorphShare, CPropertyPage)
+
+BEGIN_MESSAGE_MAP(CPPgMorphShare, CPropertyPage)
+	ON_WM_HSCROLL()
+    ON_WM_DESTROY()
+	ON_MESSAGE(WM_TREEOPTSCTRL_NOTIFY, OnTreeOptsCtrlNotify)
+END_MESSAGE_MAP()
+
+CPPgMorphShare::CPPgMorphShare()
+	: CPropertyPage(CPPgMorphShare::IDD)
+	, m_ctrlTreeOptions(theApp.m_iDfltImageListColorFlags)
+{
+	m_bInitializedTreeOpts = false;
+	m_htiHideOS = NULL;	//MORPH - Added by SiRoB, SLUGFILLER: hideOS
+	m_htiSelectiveShare = NULL;	//MORPH - Added by SiRoB, SLUGFILLER: hideOS
+	m_htiShareOnlyTheNeed = NULL; //MORPH - Added by SiRoB, SHARE_ONLY_THE_NEED
+	m_htiPowerShareLimit = NULL; //MORPH - Added by SiRoB, POWERSHARE Limit
+	//MORPH START - Added by SiRoB, Avoid misusing of powersharing	
+	m_htiPowershareMode = NULL;
+	m_htiPowershareDisabled = NULL;
+	m_htiPowershareActivated = NULL;
+	m_htiPowershareAuto = NULL;
+	m_htiPowershareLimited = NULL;
+	//MORPH END   - Added by SiRoB, Avoid misusing of powersharing	
+	//MORPH START - Added by SiRoB, Show Permission
+	m_htiPermissions = NULL;
+	m_htiPermAll = NULL;
+	m_htiPermFriend = NULL;
+	m_htiPermNone = NULL;
+	// Mighty Knife: Community visible filelist
+	m_htiPermCommunity = NULL;
+	// [end] Mighty Knife
+	//MORPH END   - Added by SiRoB, Show Permission
+}
+
+CPPgMorphShare::~CPPgMorphShare()
+{
+}
+
+void CPPgMorphShare::DoDataExchange(CDataExchange* pDX)
+{
+	CPropertyPage::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_MORPH_OPTS, m_ctrlTreeOptions);
+	if (!m_bInitializedTreeOpts)
+	{
+		int iImgSFM = 8;
+		int iImgPerm = 8;
+		int iImgPS = 8;
+
+		CImageList* piml = m_ctrlTreeOptions.GetImageList(TVSIL_NORMAL);
+		if (piml){
+			iImgSFM = piml->Add(CTempIconLoader(_T("SHAREDFILES")));
+			iImgPS = piml->Add(CTempIconLoader(_T("PREF_FILES"))); //MORPH - Added by SiRoB, POWERSHARE Limit
+		}
+		
+		CString Buffer;
+		m_htiSFM = m_ctrlTreeOptions.InsertGroup(GetResString(IDS_SFM), iImgSFM, TVI_ROOT);
+		//MORPH START - Added by SiRoB, SLUGFILLER: hideOS
+		m_htiHideOS = m_ctrlTreeOptions.InsertItem(GetResString(IDS_HIDEOVERSHARES), TREEOPTSCTRLIMG_EDIT, TREEOPTSCTRLIMG_EDIT, m_htiSFM);
+		m_ctrlTreeOptions.AddEditBox(m_htiHideOS, RUNTIME_CLASS(CNumTreeOptionsEdit));
+		m_htiSelectiveShare = m_ctrlTreeOptions.InsertCheckBox(GetResString(IDS_SELECTIVESHARE), m_htiHideOS, m_iSelectiveShare);
+		//MORPH END   - Added by SiRoB, SLUGFILLER: hideOS
+		//MORPH START - Added by SiRoB, SHARE_ONLY_THE_NEED
+		m_htiShareOnlyTheNeed = m_ctrlTreeOptions.InsertCheckBox(GetResString(IDS_SHAREONLYTHENEED), m_htiSFM, m_iShareOnlyTheNeed);
+		//MORPH END   - Added by SiRoB, SHARE_ONLY_THE_NEED
+
+		//MORPH START - Added by SiRoB, Avoid misusing of powersharing
+		m_htiPowershareMode = m_ctrlTreeOptions.InsertGroup(GetResString(IDS_POWERSHARE), iImgPS, m_htiSFM);
+		m_htiPowershareDisabled = m_ctrlTreeOptions.InsertRadioButton(GetResString(IDS_POWERSHARE_DISABLED), m_htiPowershareMode, m_iPowershareMode == 0);
+		m_htiPowershareActivated =  m_ctrlTreeOptions.InsertRadioButton(GetResString(IDS_POWERSHARE_ACTIVATED), m_htiPowershareMode, m_iPowershareMode == 1);
+		m_htiPowershareAuto =  m_ctrlTreeOptions.InsertRadioButton(GetResString(IDS_POWERSHARE_AUTO), m_htiPowershareMode, m_iPowershareMode == 2);
+		//MORPH END   - Added by SiRoB, Avoid misusing of powersharing
+		//MORPH START - Added by SiRoB, POWERSHARE Limit
+		m_htiPowershareLimited =  m_ctrlTreeOptions.InsertRadioButton(GetResString(IDS_POWERSHARE_LIMITED), m_htiPowershareMode, m_iPowershareMode == 3);
+		m_htiPowerShareLimit = m_ctrlTreeOptions.InsertItem(GetResString(IDS_POWERSHARE_LIMIT), TREEOPTSCTRLIMG_EDIT, TREEOPTSCTRLIMG_EDIT, m_htiPowershareLimited );
+		m_ctrlTreeOptions.AddEditBox(m_htiPowerShareLimit, RUNTIME_CLASS(CNumTreeOptionsEdit));
+		//MORPH END   - Added by SiRoB, POWERSHARE Limit
+		
+		//MORPH START - Added by SiRoB, Show Permission
+		m_htiPermissions = m_ctrlTreeOptions.InsertGroup(GetResString(IDS_PERMISSION), iImgPerm, m_htiSFM);
+		m_htiPermAll = m_ctrlTreeOptions.InsertRadioButton(GetResString(IDS_FSTATUS_PUBLIC), m_htiPermissions, m_iPermissions == 0);
+		m_htiPermFriend = m_ctrlTreeOptions.InsertRadioButton(GetResString(IDS_FSTATUS_FRIENDSONLY), m_htiPermissions, m_iPermissions == 1);
+		m_htiPermNone = m_ctrlTreeOptions.InsertRadioButton(GetResString(IDS_HIDDEN), m_htiPermissions, m_iPermissions == 2);
+		// Mighty Knife: Community visible filelist
+		m_htiPermCommunity = m_ctrlTreeOptions.InsertRadioButton(GetResString(IDS_COMMUNITY), m_htiPermissions, m_iPermissions == 3);
+		// [end] Mighty Knife
+		//MORPH END   - Added by SiRoB, Show Permission
+
+		m_ctrlTreeOptions.Expand(m_htiSFM, TVE_EXPAND);
+		m_ctrlTreeOptions.Expand(m_htiHideOS, TVE_EXPAND);
+		m_ctrlTreeOptions.SendMessage(WM_VSCROLL, SB_TOP);
+		m_bInitializedTreeOpts = true;
+	}
+	//MORPH START - Added by SiRoB, SLUGFILLER: hideOS
+	DDX_TreeEdit(pDX, IDC_MORPH_OPTS, m_htiHideOS, m_iHideOS);
+	DDV_MinMaxInt(pDX, m_iHideOS, 0, INT_MAX);
+	DDX_TreeCheck(pDX, IDC_MORPH_OPTS, m_htiSelectiveShare, m_iSelectiveShare);
+	//MORPH END - Added by SiRoB, SLUGFILLER: hideOS
+	//MORPH START - Added by SiRoB, SHARE_ONLY_THE_NEED
+	DDX_TreeCheck(pDX, IDC_MORPH_OPTS, m_htiShareOnlyTheNeed, m_iShareOnlyTheNeed);
+	//MORPH END   - Added by SiRoB, SHARE_ONLY_THE_NEED
+	//MORPH START - Added by SiRoB, POWERSHARE Limit
+	DDX_TreeEdit(pDX, IDC_MORPH_OPTS, m_htiPowerShareLimit, m_iPowerShareLimit);
+	DDV_MinMaxInt(pDX, m_iShareOnlyTheNeed, 0, INT_MAX);
+	//MORPH END   - Added by SiRoB, POWERSHARE Limit
+	//MORPH START - Added by SiRoB, Avoid misusing of powersharing
+	DDX_TreeRadio(pDX, IDC_MORPH_OPTS, m_htiPowershareMode, m_iPowershareMode);
+	//MORPH END   - Added by SiRoB, Avoid misusing of powersharing
+	//MORPH START - Added by SiRoB, Show Permission
+	DDX_TreeRadio(pDX, IDC_MORPH_OPTS, m_htiPermissions, m_iPermissions);
+	//MORPH END   - Added by SiRoB, Show Permission
+}
+
+
+BOOL CPPgMorphShare::OnInitDialog()
+{
+	m_iPowershareMode = thePrefs.m_iPowershareMode;//MORPH - Added by SiRoB, Avoid misusing of powersharing
+	m_iHideOS = thePrefs.hideOS; //MORPH - Added by SiRoB, SLUGFILLER: hideOS
+	m_iSelectiveShare = thePrefs.selectiveShare; //MORPH - Added by SiRoB, SLUGFILLER: hideOS
+	m_iShareOnlyTheNeed = thePrefs.ShareOnlyTheNeed; //MORPH - Added by SiRoB, SHARE_ONLY_THE_NEED
+	m_iPowerShareLimit = thePrefs.PowerShareLimit; //MORPH - Added by SiRoB, POWERSHARE Limit
+	m_iPermissions = thePrefs.permissions; //MORPH - Added by SiRoB, Show Permission
+	
+	CPropertyPage::OnInitDialog();
+	Localize();
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// EXCEPTION: OCX Property Pages should return FALSE
+}
+
+BOOL CPPgMorphShare::OnKillActive()
+{
+	// if prop page is closed by pressing VK_ENTER we have to explicitly commit any possibly pending
+	// data from an open edit control
+	m_ctrlTreeOptions.HandleChildControlLosingFocus();
+	return CPropertyPage::OnKillActive();
+}
+
+BOOL CPPgMorphShare::OnApply()
+{
+	// if prop page is closed by pressing VK_ENTER we have to explicitly commit any possibly pending
+	// data from an open edit control
+	m_ctrlTreeOptions.HandleChildControlLosingFocus();
+	
+	if (!UpdateData())
+		return FALSE;
+	thePrefs.m_iPowershareMode = m_iPowershareMode;//MORPH - Added by SiRoB, Avoid misusing of powersharing
+	thePrefs.hideOS = m_iHideOS;	//MORPH - Added by SiRoB, SLUGFILLER: hideOS
+	thePrefs.selectiveShare = m_iSelectiveShare; //MORPH - Added by SiRoB, SLUGFILLER: hideOS
+	thePrefs.ShareOnlyTheNeed = m_iShareOnlyTheNeed; //MORPH - Added by SiRoB, SHARE_ONLY_THE_NEED
+	//MORPH START - Added by SiRoB, POWERSHARE Limit
+	thePrefs.PowerShareLimit = m_iPowerShareLimit;
+	theApp.sharedfiles->UpdatePartsInfo();
+	//MORPH END   - Added by SiRoB, POWERSHARE Limit
+	thePrefs.permissions = m_iPermissions; //MORPH - Added by SiRoB, Show Permission
+
+	//theApp.scheduler->SaveOriginals(); //Added by SiRoB, Fix for Param used in scheduler
+
+	SetModified(FALSE);
+
+
+	return CPropertyPage::OnApply();
+}
+
+void CPPgMorphShare::OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar) 
+{
+	SetModified(TRUE);
+	CSliderCtrl* slider =(CSliderCtrl*)pScrollBar;
+	CString temp;
+}
+
+
+void CPPgMorphShare::Localize(void)
+{	
+	if(m_hWnd)
+	{
+		GetDlgItem(IDC_WARNINGMORPH)->SetWindowText(GetResString(IDS_WARNINGMORPH));
+		CString Buffer;
+		if (m_htiHideOS) m_ctrlTreeOptions.SetEditLabel(m_htiHideOS, GetResString(IDS_HIDEOVERSHARES));//MORPH - Added by SiRoB, SLUGFILLER: hideOS
+		if (m_htiSelectiveShare) m_ctrlTreeOptions.SetItemText(m_htiSelectiveShare, GetResString(IDS_SELECTIVESHARE));//MORPH - Added by SiRoB, SLUGFILLER: hideOS
+		if (m_htiShareOnlyTheNeed) m_ctrlTreeOptions.SetItemText(m_htiShareOnlyTheNeed, GetResString(IDS_SHAREONLYTHENEED));//MORPH - Added by SiRoB, SHARE_ONLY_THE_NEED
+		//MORPH START - Added by SiRoB, Avoid misusing of powersharing
+		if (m_htiPowershareMode) m_ctrlTreeOptions.SetItemText(m_htiPowershareMode, GetResString(IDS_POWERSHARE));
+		if (m_htiPowershareDisabled) m_ctrlTreeOptions.SetItemText(m_htiPowershareDisabled, GetResString(IDS_POWERSHARE_DISABLED));
+		if (m_htiPowershareActivated) m_ctrlTreeOptions.SetItemText(m_htiPowershareActivated, GetResString(IDS_POWERSHARE_ACTIVATED));
+		if (m_htiPowershareAuto) m_ctrlTreeOptions.SetItemText(m_htiPowershareAuto, GetResString(IDS_POWERSHARE_AUTO));
+		if (m_htiPowershareLimited) m_ctrlTreeOptions.SetItemText(m_htiPowershareLimited, GetResString(IDS_POWERSHARE_LIMITED));
+		//MORPH START - Added by SiRoB, POWERSHARE Limit
+		if (m_htiPowerShareLimit) m_ctrlTreeOptions.SetEditLabel(m_htiPowerShareLimit, GetResString(IDS_POWERSHARE_LIMIT));
+		//MORPH END   - Added by SiRoB, POWERSHARE Limit
+		//MORPH START - Added by SiRoB, Show Permission
+		if (m_htiPermissions) m_ctrlTreeOptions.SetItemText(m_htiPermissions, GetResString(IDS_PERMISSION));
+		if (m_htiPermAll) m_ctrlTreeOptions.SetItemText(m_htiPermAll, GetResString(IDS_FSTATUS_PUBLIC));
+		if (m_htiPermFriend) m_ctrlTreeOptions.SetItemText(m_htiPermFriend, GetResString(IDS_FSTATUS_FRIENDSONLY));
+		if (m_htiPermNone) m_ctrlTreeOptions.SetItemText(m_htiPermNone, GetResString(IDS_HIDDEN));
+		// Mighty Knife: Community visible filelist
+		if (m_htiPermCommunity) m_ctrlTreeOptions.SetItemText(m_htiPermCommunity, GetResString(IDS_COMMUNITY));
+		// [end] Mighty Knife
+		//MORPH END   - Added by SiRoB, Show Permission
+
+	}
+
+}
+
+void CPPgMorphShare::OnDestroy()
+{
+	m_ctrlTreeOptions.DeleteAllItems();
+	m_ctrlTreeOptions.DestroyWindow();
+	m_bInitializedTreeOpts = false;
+	m_htiHideOS = NULL;	//MORPH - Added by SiRoB, SLUGFILLER: hideOS
+	m_htiSelectiveShare = NULL;	//MORPH - Added by SiRoB, SLUGFILLER: hideOS
+	m_htiShareOnlyTheNeed = NULL; //MORPH - Added by SiRoB, SHARE_ONLY_THE_NEED
+	m_htiPowerShareLimit = NULL; //MORPH - Added by SiRoB, POWERSHARE Limit
+	//MORPH START - Added by SiRoB, Avoid misusing of powersharing	
+	m_htiPowershareMode = NULL;
+	m_htiPowershareDisabled = NULL;
+	m_htiPowershareActivated = NULL;
+	m_htiPowershareAuto = NULL;
+	m_htiPowershareLimited = NULL;
+	//MORPH END   - Added by SiRoB, Avoid misusing of powersharing	
+	//MORPH START - Added by SiRoB, Show Permission
+	m_htiPermissions = NULL;
+	m_htiPermAll = NULL;
+	m_htiPermFriend = NULL;
+	m_htiPermNone = NULL;
+	m_htiPermCommunity = NULL;
+	//MORPH END   - Added by SiRoB, Show Permission
+	CPropertyPage::OnDestroy();
+}
+LRESULT CPPgMorphShare::OnTreeOptsCtrlNotify(WPARAM wParam, LPARAM lParam)
+{
+	if (wParam == IDC_MORPH_OPTS){
+		TREEOPTSCTRLNOTIFY* pton = (TREEOPTSCTRLNOTIFY*)lParam;
+		//		if (bCheck && m_ctrlTreeOptions.GetCheckBox(m_htiUSSEnabled,bCheck))
+		//			if (bCheck) m_ctrlTreeOptions.SetCheckBox(m_htiUSSEnabled,false);
+		//}else if (pton->hItem == m_htiUSSEnabled){
+		//	BOOL bCheck;
+		//	if (m_ctrlTreeOptions.GetCheckBox(m_htiUSSEnabled, bCheck))
+		//		if (bCheck && m_ctrlTreeOptions.GetCheckBox(m_htiSUCEnabled,bCheck))
+		//			if (bCheck) m_ctrlTreeOptions.SetCheckBox(m_htiSUCEnabled,false);
+		//	
+		//}
+		SetModified();
+	}
+	return 0;
+}
---
+++ srchybrid/PPgMorphShare.h
@@ -0,0 +1,62 @@
+#pragma once
+#include "TreeOptionsCtrlEx.h"
+// CPPgMorphShare dialog
+
+class CPPgMorphShare : public CPropertyPage
+{
+	DECLARE_DYNAMIC(CPPgMorphShare)
+
+public:
+	CPPgMorphShare();
+	virtual ~CPPgMorphShare();
+
+// Dialog Data
+	enum { IDD = IDD_PPG_MORPH_SHARE };
+protected:
+
+	int m_iPowershareMode; //MORPH - Added by SiRoB, Avoid misusing of powersharing
+	int m_iHideOS;	//MORPH - Added by SiRoB, SLUGFILLER: hideOS
+	int m_iSelectiveShare;  //MORPH - Added by SiRoB, SLUGFILLER: hideOS
+	int m_iShareOnlyTheNeed; //MORPH - Added by SiRoB, SHARE_ONLY_THE_NEED
+	int m_iPowerShareLimit; //MORPH - Added by SiRoB, POWERSHARE Limit
+	int m_iPermissions; //MORPH - Added by SiRoB, Show Permissions
+	
+	CTreeOptionsCtrlEx m_ctrlTreeOptions;
+	bool m_bInitializedTreeOpts;
+	HTREEITEM m_htiSFM;
+	//MORPH START - Added by SiRoB, Avoid misusing of powersharing
+	HTREEITEM m_htiPowershareMode;
+	HTREEITEM m_htiPowershareDisabled;
+	HTREEITEM m_htiPowershareActivated;
+	HTREEITEM m_htiPowershareAuto;
+	HTREEITEM m_htiPowershareLimited;
+	//MORPH END   - Added by SiRoB, Avoid misusing of powersharing
+	HTREEITEM m_htiHideOS;	//MORPH - Added by SiRoB, SLUGFILLER: hideOS
+	HTREEITEM m_htiSelectiveShare; //MORPH - Added by SiRoB, SLUGFILLER: hideOS
+	HTREEITEM m_htiShareOnlyTheNeed; //MORPH - Added by SiRoB, SHARE_ONLY_THE_NEED
+	HTREEITEM m_htiPowerShareLimit; //MORPH - Added by SiRoB, POWERSHARE Limit
+	//MORPH START - Added by SiRoB, Show Permission
+	HTREEITEM m_htiPermissions;
+	HTREEITEM m_htiPermAll;
+	HTREEITEM m_htiPermFriend;
+	HTREEITEM m_htiPermNone;
+	// Mighty Knife: Community visible filelist
+	HTREEITEM m_htiPermCommunity;
+	// [end] Mighty Knife
+	//MORPH END   - Added by SiRoB, Show Permission
+	
+	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+	afx_msg void OnDestroy();
+	afx_msg LRESULT OnTreeOptsCtrlNotify(WPARAM wParam, LPARAM lParam);
+
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+	DECLARE_MESSAGE_MAP()
+public:
+	void Localize(void);	
+	void LoadSettings(void);
+	virtual BOOL OnApply();
+	virtual BOOL OnInitDialog();
+	virtual BOOL OnKillActive();
+	afx_msg void OnSettingsChange()			{ SetModified(); }
+};
