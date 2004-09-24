9a22069345446466c1bddc3b837faf7a6b4f957c
sirob <sirob>
2004-09-24 10:58:09 +0000

    temporary change to bre able to compile :x

--- srchybrid/PPgMorph3.cpp
+++ srchybrid/PPgMorph3.cpp
@@ -13,14 +13,6 @@ static char THIS_FILE[]=__FILE__;
 #define new DEBUG_NEW
 #endif
 
-//MORPH START - Added by SiRoB / Commander, Wapserver [emulEspaña]
-#include "TreeOptsPrefs\PassTreeOptionsEdit.h"
-#include "emuledlg.h"
-#include "serverwnd.h"
-#define HIDDEN_PASSWORD _T("*****")
-//MORPH END - Added by SiRoB / Commander, Wapserver [emulEspaña]
-
-
 ///////////////////////////////////////////////////////////////////////////////
 // CPPgMorph3 dialog
 
@@ -29,34 +21,13 @@ BEGIN_MESSAGE_MAP(CPPgMorph3, CPropertyPage)
 	ON_EN_CHANGE(IDC_EDIT_DYNDNS_PASSWORD, OnDataChange)
 	ON_EN_CHANGE(IDC_EDIT_DYNDNS_HOSTNAME, OnDataChange)
 	ON_EN_CHANGE(IDC_CHECK_DYNDNS_ENABLED, OnDataChange)
-	ON_MESSAGE(WM_TREEOPTSCTRL_NOTIFY, OnTreeOptsCtrlNotify)
 	ON_WM_DESTROY()
 END_MESSAGE_MAP()
 
 IMPLEMENT_DYNAMIC(CPPgMorph3, CPropertyPage)
 CPPgMorph3::CPPgMorph3()
 : CPropertyPage(CPPgMorph3::IDD)
-, m_ctrlTreeOptions(theApp.m_iDfltImageListColorFlags)	
 {
-	m_bInitializedTreeOpts = false;
-
-
-	// Added by MoNKi [ MoNKi: -Wap Server- ]
-	m_htiWapRoot = NULL;
-	m_htiWapEnable = NULL;
-	m_htiWapPort = NULL;
-	m_htiWapTemplate = NULL;
-	m_htiWapPass = NULL;
-	m_htiWapLowEnable = NULL;
-	m_htiWapLowPass = NULL;
-	m_bWapEnable = false;
-	m_iWapPort = 0;
-	m_sWapTemplate = "";
-	m_sWapPass = "";
-	m_bWapLowEnable = false;
-	m_sWapLowPass = "";
-	//MORPH END - Added by SiRoB / Commander, Wapserver [emulEspaña]
-
 }
 
 CPPgMorph3::~CPPgMorph3()
@@ -66,63 +37,15 @@ CPPgMorph3::~CPPgMorph3()
 void CPPgMorph3::DoDataExchange(CDataExchange* pDX)
 {
 	CPropertyPage::DoDataExchange(pDX);
-	DDX_Control(pDX, IDC_MORPH3_OPTS, m_ctrlTreeOptions);
-	if ( !m_bInitializedTreeOpts )
-	{
-		CImageList* piml = m_ctrlTreeOptions.GetImageList(TVSIL_NORMAL);
-
-
-		//MORPH START - Added by SiRoB / Commander, Wapserver [emulEspaña]
-		int iImgWap = 8; // default icon
-		if (piml){
-			iImgWap = piml->Add(CTempIconLoader(_T("MOBILE")));
-		}
-		m_htiWapRoot = m_ctrlTreeOptions.InsertItem(_T("Wap Interface"), iImgWap, iImgWap, TVI_ROOT);
-		m_htiWapEnable  = m_ctrlTreeOptions.InsertCheckBox(_T("Enable Wap Interface"), m_htiWapRoot, m_bWapEnable);
-		m_htiWapTemplate = m_ctrlTreeOptions.InsertItem(_T("Template"), TREEOPTSCTRLIMG_EDIT, TREEOPTSCTRLIMG_EDIT, m_htiWapRoot);
-		m_ctrlTreeOptions.AddFileEditBox(m_htiWapTemplate,RUNTIME_CLASS(CTreeOptionsEdit), RUNTIME_CLASS(CTreeOptionsBrowseButton));
-		m_htiWapPort = m_ctrlTreeOptions.InsertItem(_T("Port"), TREEOPTSCTRLIMG_EDIT, TREEOPTSCTRLIMG_EDIT, m_htiWapRoot);
-		m_ctrlTreeOptions.AddEditBox(m_htiWapPort, RUNTIME_CLASS(CNumTreeOptionsEdit));
-		m_htiWapPass = m_ctrlTreeOptions.InsertItem(_T("Pass"), TREEOPTSCTRLIMG_EDIT, TREEOPTSCTRLIMG_EDIT, m_htiWapRoot);
-		m_ctrlTreeOptions.AddPassEditBox(m_htiWapPass, RUNTIME_CLASS(CPassTreeOptionsEdit));
-		m_htiWapLowEnable  = m_ctrlTreeOptions.InsertCheckBox(_T("Low Enable"), m_htiWapRoot, m_bWapLowEnable);
-		m_htiWapLowPass = m_ctrlTreeOptions.InsertItem(_T("Low Pass"), TREEOPTSCTRLIMG_EDIT, TREEOPTSCTRLIMG_EDIT, m_htiWapRoot);
-		m_ctrlTreeOptions.AddPassEditBox(m_htiWapLowPass, RUNTIME_CLASS(CPassTreeOptionsEdit));
-
-		m_ctrlTreeOptions.Expand(m_htiWapRoot, TVE_EXPAND);
-		//MORPH END - Added by SiRoB / Commander, Wapserver [emulEspaña]
-
-		m_ctrlTreeOptions.SelectItem(m_ctrlTreeOptions.GetRootItem());
-		m_bInitializedTreeOpts = true;
-	}
-
-	// Added by MoNKi [ MoNKi: -Wap Server- ]
-	DDX_TreeCheck(pDX, IDC_MORPH3_OPTS, m_htiWapEnable, m_bWapEnable);
-	DDX_TreeEdit(pDX, IDC_MORPH3_OPTS, m_htiWapPort, m_iWapPort);
-	DDV_MinMaxInt(pDX, m_iWapPort, 0, 0xFFFF);
-	DDX_TreeEdit(pDX, IDC_MORPH3_OPTS, m_htiWapTemplate, m_sWapTemplate);
-	DDX_TreeEdit(pDX, IDC_MORPH3_OPTS, m_htiWapPass, m_sWapPass);
-	DDX_TreeCheck(pDX, IDC_MORPH3_OPTS, m_htiWapLowEnable, m_bWapLowEnable);
-	DDX_TreeEdit(pDX, IDC_MORPH3_OPTS, m_htiWapLowPass, m_sWapLowPass);
-	//MORPH END - Added by SiRoB / Commander, Wapserver [emulEspaña]
 }
 
 // CPPgMorph3 message handlers
 
 BOOL CPPgMorph3::OnInitDialog()
 {
-    // Added by by MoNKi [MoNKi: -Wap Server-]
-	m_bWapEnable = thePrefs.GetWapServerEnabled();
-	m_iWapPort = thePrefs.GetWapPort();
-	m_sWapTemplate = thePrefs.GetWapTemplate();
-	m_sWapPass = HIDDEN_PASSWORD;
-	m_bWapLowEnable = thePrefs.GetWapIsLowUserEnabled();
-	m_sWapLowPass = HIDDEN_PASSWORD;
-	//MORPH END - Added by SiRoB / Commander, Wapserver [emulEspaña]
-
 	CPropertyPage::OnInitDialog();
 	InitWindowStyles(this);
-    
+    /*
 	//MORPH START - Added by Commander, Various Symbols
 	if ( theApp.emuledlg->m_fontMarlett.m_hObject )
 	{
@@ -157,25 +80,18 @@ BOOL CPPgMorph3::OnInitDialog()
 		CheckDlgButton(IDC_CHECK_DYNDNS_ENABLED,1);
 	else
 		CheckDlgButton(IDC_CHECK_DYNDNS_ENABLED,0);
-
+	*/
 	Localize();
 
 	return TRUE;  // return TRUE unless you set the focus to a control
 	// EXCEPTION: OCX Property Pages should return FALSE
 }
 
-BOOL CPPgMorph3::OnKillActive()
-{
-	// if prop page is closed by pressing VK_ENTER we have to explicitly commit any possibly pending
-	// data from an open edit control
-	m_ctrlTreeOptions.HandleChildControlLosingFocus();
-	return CPropertyPage::OnKillActive();
-}
-
 BOOL CPPgMorph3::OnApply()
 {
 	if(m_bModified)
 	{   
+		/*
 		CString sBuf;
 		CString oldUsername=thePrefs.GetDynDNSUsername();
 		CString oldHostname=thePrefs.GetDynDNSHostname();
@@ -196,38 +112,13 @@ BOOL CPPgMorph3::OnApply()
 		GetDlgItem(IDC_CHECK_DYNDNS_ENABLED)->GetWindowText(sBuf);
 		if (_tstoi(sBuf)!=oldState)
 			thePrefs.SetWSPort(_tstoi(sBuf));
-		
+		*/
 	}
-
-	// if prop page is closed by pressing VK_ENTER we have to explicitly commit any possibly pending
-	// data from an open edit control
-	m_ctrlTreeOptions.HandleChildControlLosingFocus();
 
 	if (!UpdateData())
 		return FALSE;
 
 	bool bRestartApp = false;
-
-
-	// Added by by MoNKi [MoNKi: -Wap Server-]
-	thePrefs.SetWapServerEnabled(m_bWapEnable);
-	thePrefs.SetWapIsLowUserEnabled(m_bWapLowEnable);
-	if(m_sWapPass != CString(HIDDEN_PASSWORD))
-		thePrefs.SetWapPass(m_sWapPass);
-	if(m_sWapLowPass != CString(HIDDEN_PASSWORD))
-		thePrefs.SetWapLowPass(m_sWapLowPass);
-	if(m_sWapTemplate != thePrefs.GetWapTemplate()){
-		thePrefs.SetWapTemplate(m_sWapTemplate);
-		theApp.wapserver->ReloadTemplates();
-	}
-	if(m_iWapPort != thePrefs.GetWapPort()){
-		thePrefs.SetWapPort(m_iWapPort);
-		theApp.wapserver->RestartServer();
-	}
-	theApp.wapserver->StartServer();
-	theApp.emuledlg->serverwnd->UpdateMyInfo();
-	//MORPH END - Added by SiRoB / Commander, Wapserver [emulEspaña]
-	
 
 	SetModified(FALSE);
 	return CPropertyPage::OnApply();
@@ -245,15 +136,6 @@ void CPPgMorph3::Localize(void)
 		GetDlgItem(IDC_BUTTON_DYNDNS_UPDATE)->SetWindowText(GetResString(IDS_BUTTON_DYNDNS_UPDATE));
 		GetDlgItem(IDC_BUTTON_DYNDNS_RESET)->SetWindowText(GetResString(IDS_BUTTON_DYNDNS_RESET));
 
-		// Added by MoNKi [ MoNKi: -Wap Server- ]
-		if (m_htiWapRoot)		m_ctrlTreeOptions.SetItemText(m_htiWapRoot, GetResString(IDS_PW_WAP));
-		if (m_htiWapEnable)		m_ctrlTreeOptions.SetItemText(m_htiWapEnable, GetResString(IDS_ENABLED));
-		if (m_htiWapPort)		m_ctrlTreeOptions.SetEditLabel(m_htiWapPort, GetResString(IDS_PORT));
-		if (m_htiWapTemplate)	m_ctrlTreeOptions.SetEditLabel(m_htiWapTemplate, GetResString(IDS_WS_RELOAD_TMPL));
-		if (m_htiWapPass)		m_ctrlTreeOptions.SetEditLabel(m_htiWapPass, GetResString(IDS_WS_PASS));
-		if (m_htiWapLowEnable)	m_ctrlTreeOptions.SetItemText(m_htiWapLowEnable, GetResString(IDS_WEB_LOWUSER));
-		if (m_htiWapLowPass)	m_ctrlTreeOptions.SetEditLabel(m_htiWapLowPass, GetResString(IDS_WS_PASS));
-		//MORPH END - Added by SiRoB / Commander, Wapserver [emulEspaña]
 	}
 }
 
@@ -268,32 +150,7 @@ void CPPgMorph3::OnEnChangeDynDNSEnabled()
 	SetModified();
 }
 
-LRESULT CPPgMorph3::OnTreeOptsCtrlNotify(WPARAM wParam, LPARAM lParam)
-{
-	if (wParam == IDC_MORPH3_OPTS)
-	{
-		TREEOPTSCTRLNOTIFY* pton = (TREEOPTSCTRLNOTIFY*)lParam;
-
-		SetModified();
-	}
-	return 0;
-}
-
 void CPPgMorph3::OnDestroy()
 {
-	m_ctrlTreeOptions.DeleteAllItems();
-	m_ctrlTreeOptions.DestroyWindow();
-	m_bInitializedTreeOpts = false;
-
-	// Added by MoNKi [ MoNKi: -Wap Server- ]
-	m_htiWapRoot = NULL;
-	m_htiWapEnable = NULL;
-	m_htiWapPort = NULL;
-	m_htiWapTemplate = NULL;
-	m_htiWapPass = NULL;
-	m_htiWapLowEnable = NULL;
-	m_htiWapLowPass = NULL;
-	//MORPH END - Added by SiRoB / Commander, Wapserver [emulEspaña]
-
 	CPropertyPage::OnDestroy();
 }
--- srchybrid/PPgMorph3.h
+++ srchybrid/PPgMorph3.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "TreeOptionsCtrlEx.h"
 
 class CPPgMorph3 : public CPropertyPage
 {
@@ -16,29 +15,10 @@ public:
 
 protected:
 	bool m_bModified;
-    bool	m_bInitializedTreeOpts;
-	CTreeOptionsCtrlEx	m_ctrlTreeOptions;
-
-	// Added by MoNKi [ MoNKi: -Wap Server- ]
-	HTREEITEM	m_htiWapRoot;
-	HTREEITEM	m_htiWapEnable;
-	HTREEITEM	m_htiWapPort;
-	HTREEITEM	m_htiWapTemplate;
-	HTREEITEM	m_htiWapPass;
-	HTREEITEM	m_htiWapLowEnable;
-	HTREEITEM	m_htiWapLowPass;
-	BOOL		m_bWapEnable;
-	int			m_iWapPort;
-	CString		m_sWapTemplate;
-	CString		m_sWapPass;
-	BOOL		m_bWapLowEnable;
-	CString		m_sWapLowPass;
-	//MORPH END - Added by SiRoB / Commander, Wapserver [emulEspaña]
 
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 	virtual BOOL OnInitDialog();
 	virtual BOOL OnApply();
-	virtual BOOL OnKillActive();
 
 	void SetModified(BOOL bChanged = TRUE){
 		m_bModified = bChanged;
@@ -46,7 +26,6 @@ protected:
 	}
 
 	DECLARE_MESSAGE_MAP()
-	afx_msg LRESULT OnTreeOptsCtrlNotify(WPARAM wParam, LPARAM lParam);
 	afx_msg void OnSettingsChange() {SetModified();}
     afx_msg void OnDestroy();
 	afx_msg void OnDataChange()		{SetModified();}
