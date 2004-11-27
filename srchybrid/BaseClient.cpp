cbb3d58e48f21f135de43a9d6c64f0cea1c85e09
sirob <sirob>
2004-11-27 21:39:47 +0000

    Removed: Resend partstatus from pawcio

--- srchybrid/BaseClient.cpp
+++ srchybrid/BaseClient.cpp
@@ -1421,6 +1421,15 @@ bool CUpDownClient::Disconnected(LPCTSTR pszReason, bool bFromSocket){
 		return true;
 	}
 	else{
+		//MORPH START - Added by SIRoB, ReAsk state -Patch-
+		if (m_nDownloadState==DS_CONNECTED && m_nUploadState==US_ONUPLOADQUEUE)
+		{
+			if(GetRemoteQueueRank() && !IsRemoteQueueFull())
+				SetDownloadState(DS_ONQUEUE);
+			else
+				SetDownloadState(DS_NONE);
+		}
+		//MORPH END  - Added by SiRoB, ReAsk state -Patch-
 		if (thePrefs.GetDebugClientTCPLevel() > 0)
 			Debug(_T("--- Disconnected client       %s; Reason=%s\n"), DbgGetClientInfo(true), pszReason);
 		m_fHashsetRequesting = 0;
@@ -1749,31 +1758,6 @@ void CUpDownClient::ConnectionEstablished()
 		case US_WAITCALLBACK:
 			if (theApp.uploadqueue->IsDownloading(this))
 			{
-			//MORPH START - Added by SiROB, Pawcio: PowerShare
-			CKnownFile* reqfile = theApp.sharedfiles->GetFileByID(GetUploadFileID());
-			if (reqfile){
-				CSafeMemFile data(16+16);
-				data.WriteHash16(reqfile->GetFileHash());
-				bool send = true;
-				if (reqfile->IsPartFile()){
-					((CPartFile*)reqfile)->WritePartStatus(&data, this);	// SLUGFILLER: hideOS
-					send = false;//No more hideos on download file reqfile->GetHideOS()>=0 ? reqfile->GetHideOS() : thePrefs.GetHideOvershares();
-				}
-				else if (!reqfile->ShareOnlyTheNeed(&data, this)) // Wistly SOTN
-					if (!reqfile->HideOvershares(&data, this))	// Slugfiller: HideOS
-						send = false;
-				if (send){
-					Packet* packet = new Packet(&data);
-					packet->opcode = OP_FILESTATUS;
-					theStats.AddUpDataOverheadFileRequest(packet->size);
-					socket->SendPacket(packet,true);
-				}
-				else {
-					BYTE* tmp = data.Detach();
-					free(tmp);
-				}
-			}
-			//MORPH END   - Added by SiROB, Pawcio: PowerShare
 				SetUploadState(US_UPLOADING);
 				if (thePrefs.GetDebugClientTCPLevel() > 0)
 					DebugSend("OP__AcceptUploadReq", this);
