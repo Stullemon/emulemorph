4c854154580a2bb6e1d4f78f6c6705e2efeab737
sirob <sirob>
2004-12-11 16:11:56 +0000

    synchr with official

--- srchybrid/PeerCacheClient.cpp
+++ srchybrid/PeerCacheClient.cpp
@@ -235,11 +235,9 @@ void CPeerCacheUpSocket::DetachFromClient()
 	if (GetClient())
 	{
         if (GetClient()->m_pPCUpSocket == this) {
+			
 			GetClient()->m_pPCUpSocket = NULL;
-            //MORPH - Removed by SiRoB, PeerCache -Fix-
-			/*
 			theApp.uploadBandwidthThrottler->RemoveFromStandardList(this);
-			*/
         }
 	}
 }
