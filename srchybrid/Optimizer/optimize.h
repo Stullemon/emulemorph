cdd1119a07d1abaf8b52d8c7284b2d83295553a4
sirob <sirob>
2005-03-17 10:01:30 +0000

    *** empty log message ***

--- srchybrid/Optimizer/optimize.h
+++ srchybrid/Optimizer/optimize.h
@@ -1,12 +1,1 @@
 #include "memcpy_amd.h"
-
-typedef struct
-{
-	BYTE		hash[16];
-} HashType;
-
-// md4cpy -- replacement for memcpy(dst,src,16)
-__inline void md4cpy(void* dst, const void* src)
-{
-	*reinterpret_cast<HashType*>(dst) = *reinterpret_cast<const HashType*>(src);
-}
