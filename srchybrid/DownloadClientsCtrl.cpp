6f4314818ae06963225eb3822719cf5addf4c765
Stulle Ich <stulleamgym@gmx.net>
2006-04-10 14:05:25 +0000

    Added Server Icon in DownloadClientsCtrl.cpp

--- srchybrid/DownloadClientsCtrl.cpp
+++ srchybrid/DownloadClientsCtrl.cpp
@@ -129,10 +129,13 @@ void CDownloadClientsCtrl::SetAllIcons()
 	m_ImageList.SetOverlayImage(m_ImageList.Add(CTempIconLoader(_T("ClientSecureOvl"))), 1);
 	//MORPH START - Added by SiRoB, More client & Credit Overlay Icon
 	m_ImageList.Add(CTempIconLoader(_T("ClientRightEdonkey")));
-	m_ImageList.Add(CTempIconLoader(_T("Morph")));
 	//MORPH START - Added by SiRoB, WebCache
 	m_ImageList.Add(CTempIconLoader(_T("WEBCACHE")));
 	//MORPH END   - Added by SiRoB, WebCache
+	//MORPH START - Added by Stulle, Server Icon
+	m_ImageList.Add(CTempIconLoader(_T("Server")));
+	//MORPH END   - Added by Stulle, Server Icon
+	m_ImageList.Add(CTempIconLoader(_T("Morph")));
 	m_ImageList.SetOverlayImage(m_ImageList.Add(CTempIconLoader(_T("ClientCreditOvl"))), 2);
 	m_ImageList.SetOverlayImage(m_ImageList.Add(CTempIconLoader(_T("ClientCreditSecureOvl"))), 3);
 	//MORPH END   - Added by SiRoB, More client & Credit Overlay Icon
@@ -351,13 +354,18 @@ void CDownloadClientsCtrl::DrawItem(LPDRAWITEMSTRUCT lpDrawItemStruct)
 					else if (client->GetClientSoft() == SO_LPHANT){
 						image = 13;
 					}
+					//MORPH START - Added by Stulle, Server Icon
+					else if(client->GetClientSoft() == SO_URL){
+						image = 18;
+					}
+					//MORPH END   - Added by Stulle, Server Icon
 					//MORPH START - Added by SiRoB, WebCache
 					else if (client->GetClientSoft() == SO_WEBCACHE) {
-						image = 18;
+						image = 17;
 					}
 					//MORPH END   - Added by SiRoB, WebCache
 					else if (client->ExtProtocolAvailable()){
-						image = client->IsMorph()?17:1; //MORPH START - Added by SiRoB, More client icon
+						image = client->IsMorph()?18:1; //MORPH START - Added by SiRoB, More client icon
 					}
 					else if (client->GetClientSoft() == SO_EDONKEY){
 						image = 15;
