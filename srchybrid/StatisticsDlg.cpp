40cb067892f1b69c56bf9e76f245515c5b0abfd0
sirob <sirob>
2006-04-13 17:26:43 +0000

    Powershare Graph display

--- srchybrid/StatisticsDlg.cpp
+++ srchybrid/StatisticsDlg.cpp
@@ -688,6 +688,7 @@ void CStatisticsDlg::SetCurrentRate(float uploadrate, float downloadrate)
 	m_dPlotDataUp[1] = uploadrate - (float)theApp.uploadqueue->GetDatarateOverHead()/*theStats.GetUpDatarateOverhead()*/ / 1024;
 	// current rate to friends
 	m_dPlotDataUp[2] = uploadrate - (float)theApp.uploadqueue->GetDatarateExcludingPowershare() / 1024;
+	if (thePrefs.IsSolidGraph()) m_dPlotDataUp[2] -= (float)theApp.uploadqueue->GetToNetworkDatarate() / 1024;
 	// current rate to friends
 	m_dPlotDataUp[3] = uploadrate - (float)theApp.uploadqueue->GetToNetworkDatarate() / 1024;
 	m_UploadOMeter.AppendPoints(m_dPlotDataUp);
