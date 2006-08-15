538cf51b89fe173cc0c6ea315e0d2988f505365f
pindakaasmod <pindakaasmod>
2006-08-15 20:08:35 +0000

    Ionix advanced webserver 8.14

---
+++ srchybrid/PPgIonixWebServer.h
@@ -0,0 +1,58 @@
+#pragma once
+//#include "HypertextCtrl.h"
+
+class CPPgIonixWebServer : public CPropertyPage
+{
+	DECLARE_DYNAMIC(CPPgIonixWebServer)
+
+public:
+	CPPgIonixWebServer();
+	virtual ~CPPgIonixWebServer();
+
+	enum { IDD = IDD_PPG_IONIXWEBSRV };
+
+	void Localize(void);
+
+	BOOL m_bIsInit;
+	// MORPH START  tabbed options [leuk_he]
+private:
+	enum eTab	{NONE, WEBSERVER,MULTIWEBSERVER};
+	CTabCtrl	m_tabCtr;
+	eTab		m_currentTab;
+	CImageList	m_imageList;
+	void		SetTab(eTab tab);
+public:
+	afx_msg void OnTcnSelchangeTab(NMHDR * /* pNMHDR*/, LRESULT *pResult);
+	void	InitTab(bool firstinit, int Page = 0);
+ // MORPH END tabbed options [leuk_he]
+protected:
+	BOOL m_bModified;
+
+	void LoadSettings(void);
+
+	virtual void DoDataExchange(CDataExchange* pDX);
+	virtual BOOL OnInitDialog();
+	virtual BOOL OnApply();
+	virtual BOOL OnCommand(WPARAM wParam, LPARAM lParam);
+	virtual BOOL OnSetActive(); 
+	void SetModified(BOOL bChanged = TRUE){
+		m_bModified = bChanged;
+		CPropertyPage::SetModified(bChanged);
+	}
+
+	DECLARE_MESSAGE_MAP()	
+	afx_msg void OnHelp();
+	afx_msg BOOL OnHelpInfo(HELPINFO* pHelpInfo);
+//>>> [ionix] - iONiX::Advanced WebInterface Account Management
+	afx_msg void OnSettingsChange();
+	afx_msg void OnSettingsChangeBox()			{ SetBoxes(); OnSettingsChange(); }
+	afx_msg void OnBnClickedNew();
+	afx_msg void OnBnClickedDel();
+	afx_msg void UpdateSelection();
+	CComboBox	m_cbAccountSelector;
+	CComboBox	m_cbUserlevel;
+	void	SetBoxes();
+	void	FillComboBox();
+	void	FillUserlevelBox();
+//<<< [ionix] - iONiX::Advanced WebInterface Account Management
+};
