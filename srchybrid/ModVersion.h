ed01550eae8990ff91cf83c90947e96983ea94bc
Stulle Ich <stulleamgym@gmx.net>
2010-04-18 23:54:24 +0200

    updated German and Chinese (Taiwan) translation updated changelogs removed question box asking if dump should be created for beta builds

--- srchybrid/ModVersion.h
+++ srchybrid/ModVersion.h
@@ -21,7 +21,7 @@
 //START - This part should not be changed in anyway
 #define MOD_VERSION_MJR		12
 #define MOD_VERSION_MIN		0
-#define MOD_VERSION_BUILD   5
+#define MOD_VERSION_BUILD   7
 #define	MOD_VERSION			_T("MorphXT")
 #define	MOD_VERSION_LONG	_T("MorphXT v")
 //END   - This part should not be changed in anyway
