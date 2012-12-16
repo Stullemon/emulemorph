9462358f41622260030c59bd432b073f17c875b3
Stulle Ich <stulleamgym@gmx.net>
2012-12-16 02:34:42 +0100

    forgot to update mod version. :-D

--- srchybrid/ModVersion.h
+++ srchybrid/ModVersion.h
@@ -21,7 +21,7 @@
 //START - This part should not be changed in anyway
 #define MOD_VERSION_MJR		12
 #define MOD_VERSION_MIN		7
-#define MOD_VERSION_BUILD   1
+#define MOD_VERSION_BUILD   2
 #define	MOD_VERSION			_T("MorphXT")
 #define	MOD_VERSION_LONG	_T("MorphXT v")
 //END   - This part should not be changed in anyway
